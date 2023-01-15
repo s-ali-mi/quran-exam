89cfb080da034a0fa2ecd1e3916b98a8c4d43de0
Ali Salimi <85091125+0250902109@users.noreply.github.com>
2023-01-15 14:33:24 +0330

    finaly update

--- quran/settings.py
+++ quran/settings.py
@@ -117,8 +117,12 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
-
+import os
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static/'),
+]
 STATIC_URL = '/static/'
+
 Media_URL = '/static/'
 
 # Default primary key field type
